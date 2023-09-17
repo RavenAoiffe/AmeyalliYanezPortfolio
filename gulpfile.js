743c52e7c8e75e2f2fb62b40f04d07255fc9e7b8
Ameyalli <ameyanezcmm@gmail.com>
2023-09-16 21:49:50 -0600

    commit inicial proyecto react

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const {series, src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+//utilidades css
+/*const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+*/
+const paths ={
+    scss: 'src/scss/**/*.scss'
+}
+function css(){
+    return src(paths.scss)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(dest('./build/css'))
+}
+function watchArchivos(){
+    watch(paths.scss, css);
+}
+
+exports.css = css;
+exports.watchArchivos = watchArchivos;
+
+//exports.default = series(imagenes,javascript,watchArchivos);
+exports.default = series(watchArchivos);
