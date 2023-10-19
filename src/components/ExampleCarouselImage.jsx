f55ea0aaf43379a7db5d8404cf637c52d3fd3337
Ameyalli <ameyanezcmm@gmail.com>
2023-10-19 16:55:45 -0600

    key del swiper

--- src/components/ExampleCarouselImage.jsx
+++ src/components/ExampleCarouselImage.jsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 const ExampleCarouselImage = ({project}) => {
     const {image,link} = project;
-    console.log(image)
     return (
         <a href={link} target='_blank' rel='noopener'>
             <div className='flex carousel-project justify-space align-center' > 
