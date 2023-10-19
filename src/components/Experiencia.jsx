5497e649c9a6bfe76207465d91019c61a1656498
Ameyalli <ameyanezcmm@gmail.com>
2023-10-19 16:48:17 -0600

    Experiencia styles

---
+++ src/components/Experiencia.jsx
@@ -0,0 +1,89 @@
+
+const Experiencia = () => {
+    return (
+        <div className="">
+            <div className="bloque flex experiencia">
+                <div className="cuadro-primero cuadro text-center">
+                    <div className="cuadro-flotante">
+                    <h2 className="font-900">CMM</h2>
+                    </div>
+                </div>
+                <div className="imagenCuadro">
+                    <p>2018 - Actualidad</p>
+                    <ul>
+                        <li>Diseño de sitios web</li>
+                        <li>Maquetación y programación de sitios web</li>
+                        <li>HTML, CSS, JS, JQuery, Gulp, SCSS, NodeJs, ReactJs</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="bloque flex experiencia">
+            <div className="cuadro-primero cuadro text-center hidden-desktop">
+                    <div className="cuadro-flotante">
+                    <h2 className="font-900">Enapsys</h2>
+                    </div>
+                </div>  
+                <div className="imagenCuadro">
+                    <p>2021 - 2023</p>
+                    <ul>
+                        <li>Look & Feel para plataformas commerce SAP</li>
+                        <li>Trabajo conjunto con Emarsys SAP</li>
+                        <li>Manejo de CSS, LESS y Bootstrap HTML</li>
+                        <li>Jquery, Javascript Vanilla y uso de librerías</li>
+                        <li>JIRA</li>
+                        <li>Trabajo con metodologías ágiles</li>
+                        <li>Manejo de Versiones GIT / Bitbucket</li>
+                        <li>User Experience</li>
+                        <li>User Interface</li>
+                    </ul>
+                </div>
+                <div className="cuadro-primero cuadro text-center hidden-mobile">
+                    <div className="cuadro-flotante">
+                    <h2 className="font-900">Enapsys</h2>
+                    </div>
+                </div>  
+            </div>
+            <div className="bloque flex experiencia">
+                <div className="cuadro-primero cuadro text-center">
+                    <div className="cuadro-flotante">
+                    <h2 className="font-900">Aktien TI</h2>
+                    </div>
+                </div>
+                <div className="imagenCuadro">
+                    <p>2017-2018</p>
+                    <ul>
+                        <li>Maquetación en css, html y JS</li>
+                        <li>Sitios Responsivos - Bootstrap</li>
+                        <li>Creación de propuestas de Diseño Web</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="bloque flex experiencia">
+            <div className="cuadro-primero cuadro text-center hidden-desktop">
+                    <div className="cuadro-flotante">
+                    <h2 className="font-900">accesso Showare</h2>
+                    </div>
+                </div> 
+                <div className="imagenCuadro">
+                    <p>2016-2017</p>
+                    <ul>
+                        <li>Actualización Look & Feel de diversos sitios</li>
+                        <li>Implementación de Sitios en Ambientes de Desarrollo</li>
+                        <li>Traducción al Español de Diversos sitios</li>
+                        <li>QA & Testing</li>
+                        <li>Desarrollo de Funcionalidades y corrección de Errores</li>
+                        <li>Desarrollo .Net y C#</li>
+                        <li>Creación de Apps Móviles</li>
+                    </ul>
+                </div>
+                <div className="cuadro-primero cuadro text-center hidden-mobile">
+                    <div className="cuadro-flotante">
+                    <h2 className="font-900">accesso Showare</h2>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    );
+}
+
+export default Experiencia;
