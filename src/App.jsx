7eed0ed5acccf4cda7fd1d57d3ee265fac6a694c
Ameyalli <ameyanezcmm@gmail.com>
2023-11-08 11:05:56 -0600

    Ajustes de texto y proyectos en swipper

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Descripcion from './components/Descripcion';
 import Experiencia from './components/Experiencia';
 import Footer from './components/Footer';
 import Swipper from './components/Swipper';
+import Project from './components/Project';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Descripcion/>
       <h2 className='mt-8 mb-10 font-size-4rem font-900 color-darkgray'>Proyectos</h2>
       <Swipper></Swipper>
+
       <h2 className='mt-8 mb-10 font-size-4rem font-900 color-darkgray'>Experiencia</h2>
       <Experiencia id="Experiencia"></Experiencia>
     </div>
--- src/components/Descripcion.jsx
+++ src/components/Descripcion.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 
 const Descripcion = () => {
     return (
-        <div id="presentacion" className='mt-3 text description'>
+        <article id="presentacion" className='mt-3 text description'>
             <p>
                <b>
-                Apasionada de la programación web y la literatura. Soy un híbrido. Me gusta mucho el diseño UI/UX y
-                el desarrollo front end, pero también me encanta leer y a veces me he dedicado a escribir. Soy
-                fanática de la literatura de ciencia ficción.
+                Apasionada de la programación web y la literatura. Me interesa el diseño UI/UX y
+                el desarrollo frontend. Soy fanática de la literatura de ciencia ficción.
                 </b>
             </p>
             <p>
-                    Mi libro favorito es Los miserables.
+                Mi libro favorito es Los miserables de Victor Hugo.
             </p>
             <p>
                 En todos mis proyectos pongo la dedicación necesaria para realizarlos con la calidad y eficacia
-                necesaria, por lo que seguir aprendiendo y capacitándome es vital para mi.
+                necesaria. Seguir aprendiendo y capacitándome es vital para mi.
             </p>
-        </div>
+        </article>
     );
 }
 
