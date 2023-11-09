d2897c39318db8089397af4ea44ecd535d07f246
Ameyalli <ameyanezcmm@gmail.com>
2023-11-08 18:23:03 -0600

    Comment on photo

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,10 +1,10 @@
 //import image from '/img/Foto_2.JPG';
-
+/*<img  style={{display:'none'}}
+            src={image} alt="portafolio"/>*/
 const Principal = ({children}) => {
     return (
         <div className='principal flex align-center justify-center'> 
-            <img  style={{display:'none'}}
-            src={image} alt="portafolio"/>
+            
             {children}
         </div>
     );
